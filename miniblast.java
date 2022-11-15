be8a4792d09e2b3b7dbfa3bf9ccaced53f3177d7
magicalcarpet <oke.ade@gmail.com>
2022-11-15 20:44:00 +0000

    Create miniblast.java

---
+++ miniblast.java
@@ -0,0 +1,9 @@
+// Its a Megablast 
+// Bitmap Brothers
+
+Public class Miniblast{
+        public static void main(){
+               System.out.println("Pay for it, pay for it..")
+          
+        }
+}
