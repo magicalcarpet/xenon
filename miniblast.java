fce7f75da7ea7cabd3ed9314064a853ed7862f5d
Ade Oke <oke.ade@gmail.com>
2022-11-15 21:09:58 +0000

    added projectx file and modified minblast.java file

--- miniblast.java
+++ miniblast.java
@@ -1,9 +1,12 @@
 // Its a Megablast 
-// Bitmap Brothers
+// Bit
+
 
 Public class Miniblast{
         public static void main(){
-               System.out.println("Pay for it, pay for it..")
+            System.out.println("Pay for it, pay for it..");
+            System.out.println("Amiga 500 was great....");
+            System.out.println("PlayStation was outstanding....");
           
         }
 }
